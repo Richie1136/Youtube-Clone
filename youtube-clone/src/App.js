78d6fb5339ffb9694e81dc7ba1c573f428afd2d5
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-09-06 11:19:47 -0400

    Setting up routes and components

--- youtube-clone/src/App.js
+++ youtube-clone/src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import { Box } from '@mui/material'
+import NavBar from './components/navbar/NavBar';
+import Feed from './components/feed/Feed';
+import VideoDetail from './components/videodetail/VideoDetail';
+import ChannelDetail from './components/channeldetail/ChannelDetail';
+import SearchFeed from './components/searchfeed/SearchFeed';
 
 function App() {
   return (
-    <div className="App">
+    <Box sx={{ backgroundColor: '#000' }}>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<Feed />} />
+        <Route path='/video/:id' element={<VideoDetail />} />
+        <Route path='/channel/:id' element={<ChannelDetail />} />
+        <Route path='/search/:searchTerm' element={<SearchFeed />} />
+
+      </Routes>
       <h2>Youtube Clone</h2>
-    </div>
+    </Box>
   );
 }
 
