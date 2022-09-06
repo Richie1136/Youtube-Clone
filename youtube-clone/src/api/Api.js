6218aae4de6e3c9405edf491b85558316decd7c6
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-09-06 10:48:11 -0400

    Installing all neccessary dependencies

---
+++ youtube-clone/src/api/Api.js
@@ -0,0 +1,14 @@
+
+
+export const baseUrl = 'youtube-v31.p.rapidapi.com'
+
+// export const fetchApi = async (url) => {
+//   const { data } = await axios.get((url), {
+//     headers: {
+
+//       'X-RapidAPI-Key': 'e9f136fd86msh20374d953bfb05ep137d7ejsnaede58975cae',
+//       'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+//     }
+//   })
+//   return data
+// }
