9b8225c7724f5a59e3b3354ef296426f10ea7ca0
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-09-07 10:54:44 -0400

    Adding styling to the sidebar button

--- youtube-clone/src/components/sidebar/SideBar.js
+++ youtube-clone/src/components/sidebar/SideBar.js
@@ -1,12 +1,15 @@
 import { Stack } from "@mui/material"
 import { categories } from "../../utils/constants"
 
+
+const selected = 'New'
+
 const SideBar = () => {
   return (
     <Stack direction="row"
       sx={{ overflowY: 'auto', height: { sx: 'auto', md: '95%' }, flexDirection: { md: 'column' } }}>
       {categories?.map(({ name, icon }) => (
-        <button key={name}>
+        <button className='category-btn' key={name} style={{ backgroundColor: name === selected && '#FC1503', color: 'white' }}>
           <span>{icon}</span>
           <psan>{name}</psan>
         </button>
