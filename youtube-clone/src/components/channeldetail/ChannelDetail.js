3e8d74acc4654c7795362a539d35e7f5d4bbc906
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-09-09 09:57:45 -0400

    Accessing channel details using rapid api

--- youtube-clone/src/components/channeldetail/ChannelDetail.js
+++ youtube-clone/src/components/channeldetail/ChannelDetail.js
@@ -1,7 +1,35 @@
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { Box } from '@mui/material'
+import { Videos, ChannelCard } from '../index'
+import { fetchApi } from '../../api/Api'
+
 
 const ChannelDetail = () => {
+
+  const { id } = useParams()
+
+  const [channelDetail, setChannelDetail] = useState(null)
+
+
+  useEffect(() => {
+    const fetchChannelDetails = async () => {
+      try {
+        const response = await fetchApi(`channels?part="snippet"&id=${id}`)
+        setChannelDetail(response?.items[0])
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchChannelDetails()
+  }, [id])
+
+
   return (
-    <div>ChannelDetail</div>
+    <Box>
+
+      <h2>{channelDetail?.snippet?.title}</h2>
+    </Box>
   )
 }
 
