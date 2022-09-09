6fa3ebd3ac83f870c9bf5b3a3e910e37e9a65f81
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-09-09 09:42:57 -0400

    Fixed an issue with the feed that was causing it to not take up all of the avaliable width

--- youtube-clone/src/components/feed/Feed.js
+++ youtube-clone/src/components/feed/Feed.js
@@ -28,17 +28,21 @@ const Feed = () => {
 
 
   return (
+
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
-      <Box sx={{ height: { sx: 'auto', md: '92vh' }, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 } }}>
+      <Box sx={{ height: { sx: "auto", md: '92vh' }, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 } }}>
         <SideBar selected={selected} setSelected={setSelected} />
-        <Typography className='copyright' variant='body2' sx={{ mt: 1.5, color: '#fff' }}>
-          Copyright 2022 Richard Hagenah
+
+        <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
+          Copyright © Richard Hagenah
         </Typography>
       </Box>
-      <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
-        <Typography variant='h4' fontWeight='bold' mb={2} sx={{ color: 'white' }}>
-          {selected} <span style={{ color: '#f31503' }}>Videos</span>
+
+      <Box p={2} sx={{ overflowY: "auto", height: '90vh', flex: 2 }}>
+        <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+          {selected} <span style={{ color: "#FC1503" }}>videos</span>
         </Typography>
+
         <Videos videos={videos} />
       </Box>
     </Stack>
