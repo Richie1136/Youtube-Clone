c11b9f3aafb2738891f2f1f1a625174d16386569
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-09-09 10:41:30 -0400

    Added Channel profile background color

--- youtube-clone/src/components/channeldetail/ChannelDetail.js
+++ youtube-clone/src/components/channeldetail/ChannelDetail.js
@@ -10,13 +10,23 @@ const ChannelDetail = () => {
   const { id } = useParams()
 
   const [channelDetail, setChannelDetail] = useState(null)
+  const [videos, setVideos] = useState([])
+
+  console.log(channelDetail)
+  console.log(videos)
 
 
   useEffect(() => {
     const fetchChannelDetails = async () => {
       try {
-        const response = await fetchApi(`channels?part="snippet"&id=${id}`)
+        const response = await fetchApi(`channels?part=snippet&id=${id}`)
         setChannelDetail(response?.items[0])
+      } catch (error) {
+        console.log(error)
+      }
+      try {
+        const response = await fetchApi(`search?channelId=${id}&part=snippet&order=date`)
+        setVideos(response?.items)
       } catch (error) {
         console.log(error)
       }
@@ -26,7 +36,15 @@ const ChannelDetail = () => {
 
 
   return (
-    <Box>
+    <Box minHeight="95vh">
+      <Box>
+        <div style={{
+          backgroundImage: "linear-gradient(to right, #4880EC, #019CAD)",
+          zIndex: 10,
+          height: '300px'
+        }}
+        />
+      </Box>
 
       <h2>{channelDetail?.snippet?.title}</h2>
     </Box>
