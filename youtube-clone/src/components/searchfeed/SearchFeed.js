b23c9d038c4dd2228338b9b6c817a9d12448e8a2
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-09-09 11:19:20 -0400

    Able to fetch videos by the search term when using the dynamic path

--- youtube-clone/src/components/searchfeed/SearchFeed.js
+++ youtube-clone/src/components/searchfeed/SearchFeed.js
@@ -1,7 +1,45 @@
+import { useState, useEffect } from 'react'
+import { Box, Typography } from '@mui/material'
+import { Videos } from '../index'
+import { fetchApi } from '../../api/Api'
+import { useParams } from 'react-router-dom'
+
+
+
+// px - stands for horizontal padding
 
 const SearchFeed = () => {
+
+  const [videos, setVideos] = useState([])
+
+  const { searchTerm } = useParams()
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetchApi(`search?part=snippet&q=${searchTerm}`)
+        setVideos(response.items)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchData()
+  }, [searchTerm])
+
+
+
+
   return (
-    <div>SearchFeed</div>
+
+
+
+    <Box p={2} sx={{ overflowY: "auto", height: '90vh', flex: 2 }}>
+      <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+        Search Results for: <span style={{ color: "#FC1503" }}>{searchTerm}</span>
+      </Typography>
+
+      <Videos videos={videos} />
+    </Box>
   )
 }
 
